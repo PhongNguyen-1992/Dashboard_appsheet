e8b0fce53401d7e41064e21e38a92551d4244ecd
PhongNguyen-1992 <phongnguyenfpt1992@gmail.com>
2026-05-01 22:22:47 +0700

    bỏ PageShell

--- vite-project/src/pages/CapDoPage.tsx
+++ vite-project/src/pages/CapDoPage.tsx
@@ -1,5 +1,3 @@
-import PageShell from "../components/PageShell";
-
 const FORM_URL =
   "https://script.google.com/macros/s/AKfycbxJfqf_ZKT71EYaNFw7UIHzd0DItW7sUoaKkimFpdefbZjCMVTvj0RXLCTqv2Bc8qW5/exec";
 
--- vite-project/src/pages/RecarePage.tsx
+++ vite-project/src/pages/RecarePage.tsx
@@ -1,5 +1,3 @@
-import PageShell from "../components/PageShell";
-
 const FORM_URL =
   "https://script.google.com/macros/s/AKfycbxhl0g6_B_WYuOK3YqJ_MFfS0vGgbKPl2LsRKhLS5VOzdIwBAGQyJ2go2nQJnKCgzqB/exec";
 
